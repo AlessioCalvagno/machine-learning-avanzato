9e104489f8f0c7c228c2ea31aed068c4556189ac
alessio176 <alessio176@gmail.com>
2024-08-25 20:26:00 +0200

    first notebook comments

--- progetto finale/HOGFeatureExtractor.py
+++ progetto finale/HOGFeatureExtractor.py
@@ -6,19 +6,51 @@ from tqdm import tqdm
 
 class HOGFeatureExtractor(BaseEstimator, TransformerMixin):
     """
-    TODO: write docstring
+    This transformer computes feature extraction for an image or images array, through HOG method.
+    It can be used standalone or integrated inside a standard scikit-learn Pipeline.
+    The given image must be grayscale.
     """
 
     def __init__(self,orientations=9, pixels_per_cell=(8, 8), cells_per_block=(2, 2)):
+        """
+        Class constructor. Its parameters are used as configruation for hog feature extraction.
+
+        Parameters:
+        - orientations: int, default = 9
+            Number of orientation bin of HOG histogram.
+        - pixels_per_cell: 2-tuple (int, int), default = (8, 8)
+            Size (in pixels) of a cell.
+        - cells_per_block: 2-tuple (int, int), default = (2, 2)
+            Number of cells in each block.
+
+        For more info about HOG feature extraction, visit: 
+        https://www.analyticsvidhya.com/blog/2019/09/feature-engineering-images-introduction-hog-feature-descriptor/
+        """
         self.orientations = orientations
         self.pixels_per_cell = pixels_per_cell
         self.cells_per_block = cells_per_block
 
     def fit(self, X, y=None):
+        """
+        This method does nothing, since HOG feature descriptor hasn't a fit process.
+        It's included for compatibility with scikit-learn Pipeline.
+        """
         return self
     
     def transform(self, X, y=None):
-        if X.ndim == 2:  # Caso di una singola immagine
+        """
+        Perform feature extraction of the input image.
+
+        Parameters:
+        - X: (M,N) ndarray or (K,M,N) ndarray.
+            If X is a 2d array, it's a single grayscale image;
+            If X is a 3 array, it's an array of K grayscale images.
+        - y: ignored, included only for compatibility reasons.
+                
+        Return:
+        extracted features matrix as 2d array. 
+        """
+        if X.ndim == 2:  # Single image case
             return hog(X, pixels_per_cell=self.pixels_per_cell, 
                        cells_per_block=self.cells_per_block, 
                        orientations=self.orientations).reshape(1, -1)
--- progetto finale/ImageResizer.py
+++ progetto finale/ImageResizer.py
@@ -5,16 +5,41 @@ from tqdm import tqdm
 
 class ImageResizer(BaseEstimator, TransformerMixin):
     """
-    TODO: Write docstring
+    This transformer resizes input image(s), as a preprocessing step before feature extraction.
+    It can be used standalone or integrated inside a standard scikit-learn Pipeline.
+    The given image must be grayscale. 
     """
     
     def __init__(self, resize_shape = (128, 64)):
+        """
+        Class constructor. Its parameter is used as input to scikit-image resize function.
+
+        Parameter:
+        - resize_shape: 2-tuple (int, int), default (128, 64)
+            Desired final image size, as (height, width).
+        """
         self.resize_shape = resize_shape #as row x columns (h x w)
         
     def fit(self, X, y=None):
+        """
+        This method does nothing, since resizing hasn't a fit process.
+        It's included for compatibility with scikit-learn Pipeline.
+        """
         return self
     
     def transform(self, X, y=None):
+        """
+        Resize input image(s) as resize_shape ndarray.
+
+        Parameters:
+        - X: (M,N) ndarray or (K,M,N) ndarray.
+            If X is a 2d array, it's a single grayscale image;
+            If X is a 3 array, it's an array of K grayscale images.
+        - y: ignored, included only for compatibility reasons.
+
+        Return:
+        resized image as ndarray (2d if X is a single image or 3d if X is an array of images). 
+        """
         if X.ndim == 2:  # Caso di una singola immagine
             return resize(X, self.resize_shape).reshape(1, *self.resize_shape)
         # return np.array([resize(img, self.resize_shape) for img in tqdm(X, desc="Resize image", unit="item")])
